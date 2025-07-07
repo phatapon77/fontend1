3888f80ed0e9e46a5ed8fe1410a4a70ffe4164a2
phatapon77 <phatapon.opor@gmail.com>
2025-07-07 11:35:05 +0700

    4

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,11 +1,7 @@
-import Carousel from "./conponents/Carousel";
 export default function Contact() {
   return(
     <>
     <h1 className="text-4xl text-center text-black-500">Contact Page</h1></>
-
-   <Carousel/>
-
   )
 }
     
