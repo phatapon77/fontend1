8e390017d3f57437d98c19be07afce355b5665a7
phatapon77 <phatapon.opor@gmail.com>
2025-07-07 09:15:05 +0700

    1

--- app/conponents/Navigation.js
+++ app/conponents/Navigation.js
@@ -6,7 +6,11 @@ export default function Navigation() {
   return (
   <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">DAILY</a>
+     <a className="navbar-brand" href="#">
+  <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width={30} height={24} className="d-inline-block align-text-top" />
+  Bootstrap
+</a>
+
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
     </button>
