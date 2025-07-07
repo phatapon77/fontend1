2cd5abf166213eb2c1e0e9caf363d4b11d759491
phatapon77 <phatapon.opor@gmail.com>
2025-07-07 11:09:46 +0700

    1

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
-
+import Carousel from "./conponents/Carousel";
 export default function Home() {
   return(
     <>
     <h1 className="text-4xl text-center text-black-500">Home Page</h1>
+   <Carousel/>
    
     </>
   )
 }
-    
+
