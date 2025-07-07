21a793a669c94c4ff585b9c92d5e0871ba9ffec7
phatapon77 <phatapon.opor@gmail.com>
2025-07-07 12:37:15 +0700

    1

--- app/conponents/Card.js
+++ app/conponents/Card.js
@@ -10,26 +10,26 @@ export default function Card() {
         <div className="row">
             <div className="col-md-4 mb-4">
                 <div className="card">
-                <img src="/images/card/card1.jpg" className="card-img-top img-responsive" alt="..." />
+                <img src="https://i.pinimg.com/736x/66/f4/26/66f4261019a0c14085f6d690552e52a4.jpg" className="card-img-top img-responsive" alt="..." />
                 <div className="card-body">
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <p className="card-text">-</p>
                 </div>
                 </div>
             </div>
 
             <div className="col-md-4 mb-4">
             <div className="card">
-                <img src="/images/card/card1.jpg" className="card-img-top" alt="..." />
+                <img src="https://i.pinimg.com/736x/b7/42/7e/b7427e380121c68821000b876e473aba.jpg" className="card-img-top" alt="..." />
                 <div className="card-body">
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <p className="card-text">-</p>
                 </div>
                 </div>
             </div>
             <div className="col-md-4 mb-4">
             <div className="card">
-                <img src="/images/card/card1.jpg" className="card-img-top" alt="..." />
+                <img src="https://media.printler.com/media/photo/122968.jpg?rmode=crop&width=638&height=900" className="card-img-top" alt="..." />
                 <div className="card-body">
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <p className="card-text">-</p>
                 </div>
                 </div>
             </div>
--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 import Carousel from "./conponents/Carousel";
+import Card from "./conponents/Card";
+import Footer from "./conponents/Footer";
 export default function service() {
   return(
     <>
     <h1 className="text-4xl text-center text-black-500">Home Page</h1>
     <Carousel/>
+    <Card/>
+    <Footer/>
     </>
   )
 }
