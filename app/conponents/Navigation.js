679eca4d07fdbeb625076836707a1ffb768cd7b8
phatapon77 <phatapon.opor@gmail.com>
2025-07-07 08:51:54 +0700

    1

--- app/conponents/Navigation.js
+++ app/conponents/Navigation.js
@@ -6,7 +6,7 @@ export default function Navigation() {
   return (
     <nav style={{ padding: '1rem', borderBottom: '1px solid #eee' }}>
       <ul style={{ display: 'flex', gap: '1rem', listStyle: 'none' }}>
-        <li><Link href="/">5555555</Link></li>
+        <li><Link href="/">หน้าแรก</Link></li>
         <li><Link href="/about">เกี่ยวกับ</Link></li>
         <li><Link href="/service">บริการ</Link></li>
         <li><Link href="/contact">ติดต่อ</Link></li>
