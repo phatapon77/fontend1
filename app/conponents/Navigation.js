7f2758504fd0b9690fdd9826881d641f54d376dd
phatapon77 <phatapon.opor@gmail.com>
2025-07-14 08:26:19 +0700

    1

--- app/conponents/Navigation.js
+++ app/conponents/Navigation.js
@@ -29,7 +29,7 @@ export default function Navigation() {
             เกี่ยวกับเรา
           </a>
           <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="/contact">แจ้งปัญหา</a></li>
             <li><a className="dropdown-item" href="#">Another action</a></li>
             <li><hr className="dropdown-divider" /></li>
             <li><a className="dropdown-item" href="#">Something else here</a></li>
